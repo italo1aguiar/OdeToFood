ae0104c7ade5717c877c1b57429964bf5478e2d1
Italo Aguiar Do Nascimento Paulino <italo.paulino@xpi.com.br>
2021-11-10 17:44:24 -0300

    DADOS REMOTOS

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -8,7 +8,8 @@ namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-           IEnumerable<Restaurant> GetAll();
+           //IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
 
     }
     public class InMemoryRestaurantData : IRestaurantData
@@ -21,9 +22,17 @@ namespace OdeToFood.Data
                 new Restaurant{ Id = 1, Name = "La Costa", Location="Mexico", Cuisine=CuisineType.Mexican}
             };
         }
-        public IEnumerable<Restaurant> GetAll()
+        //public IEnumerable<Restaurant> GetAll()
+        //{
+        //    return from r in restaurants
+        //           orderby r.Name
+        //           select r;
+        //}
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
         {
             return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
         }
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -15,16 +15,25 @@ namespace OdeToFood.Pages.Restaurants
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
         public string Message { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
         public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
             this.restaurantData = restaurantData;       
         }
+        //public void onget()
+        //{
+            
+        //    message = config["message"];
+        //    restaurants = restaurantdata.getall();
+        //}
         public void OnGet()
         {
+
             Message = config["Message"];
-            Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
